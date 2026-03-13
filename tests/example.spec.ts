1b30f2f31d2d7945092ab8b4c4ac55872f8b493e
Yevhen Voitsytskyi <amfi1987@gmail.com>
2026-03-13 15:01:21 +0100

    tests: test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,6 +16,3 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
-
-//new file: playwright.config.ts
-// playwright.config.ts
